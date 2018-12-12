09a0550fce7f394c8d23014623270cdf01e5af12
autumnfish <517729329@qq.com>
2018-12-12 17:01:37 +0800

    完成了搜索页,跳转到详情页 详情页布局渲染

---
+++ src/pages/detail/index.vue
@@ -0,0 +1,297 @@
+<template>
+  <div class='detail-container'>
+    <!-- 轮播图 -->
+    <swiper
+      indicator-dots
+      autoplay
+      circular
+    >
+      <swiper-item
+        v-for="(item, index) in goodsInfo.pics"
+        :key="item.pics_id"
+        @click='previewImg(item.pics_big)'
+      >
+        <image
+          mode="aspectFill"
+          :src="item.pics_mid"
+        ></image>
+      </swiper-item>
+    </swiper>
+
+    <!-- 商品的基本信息 -->
+    <div class="goodsinfo">
+      <div class="price">¥{{goodsInfo.goods_price}}</div>
+      <div class="name-box">
+        <div class="left">
+          {{goodsInfo.goods_name}}
+        </div>
+        <div class="right">
+          <i class="iconfont icon-shoucang"></i>
+          收藏
+        </div>
+      </div>
+      <div class="express">
+        快递:&nbsp;&nbsp;&nbsp;免运费
+      </div>
+    </div>
+
+    <!-- 选项区域 -->
+    <div class="section">
+      <div class="item hot">
+        <span>促销</span> 满300减30元
+      </div>
+      <div class="item">
+        <span>已选</span> 黑色/S/1件
+
+      </div>
+    </div>
+    <div class="section">
+      <div
+        class="item"
+        @click="chooseAddress"
+      >送至 {{address}} <i class='iconfont icon-jiantouyou'></i></div>
+    </div>
+
+    <!-- tab栏切换 -->
+    <div class="tab-bar">
+      <div class="bar-item" @click="tabIndex=0" :class="{active:tabIndex==0}">图文介绍</div>
+      <div class="bar-item" @click="tabIndex=1" :class="{active:tabIndex==1}">规格参数</div>
+    </div>
+    <div class="tab-content">
+      <div class="content-item" v-show="tabIndex==0">1</div>
+      <div class="content-item" v-show="tabIndex==1">2</div>
+    </div>
+
+    <!-- 底部的控制器区域 -->
+    <div class="control-box">
+      <div
+        class="item"
+        @click="callKefu"
+      >
+        <i class='iconfont icon-kefu'></i>
+        联系客服
+      </div>
+      <div class="item">
+        <i class='iconfont icon-gouwuche'></i>
+        购物车
+      </div>
+      <button class="cart">加入购物车</button>
+      <button class="buy">立即购买</button>
+    </div>
+  </div>
+</template>
+<script>
+// 导入hxios
+import hxios from "../../utils/index";
+export default {
+  data: function() {
+    return {
+      //    商品id
+      id: 0,
+      // 商品详情
+      goodsInfo: {},
+      // 收货地址
+      address:'',
+      // tan显示的索引
+      tabIndex:0
+    };
+  },
+  // 事件
+  methods: {
+    previewImg(current) {
+      // 准备一个 用来预览的数组
+      let urls = [];
+      this.goodsInfo.pics.forEach(v => {
+        urls.push(v.pics_big);
+      });
+      wx.previewImage({
+        current, // 当前显示图片的http链接
+        urls // 需要预览的图片http链接列表
+      });
+    },
+    // 联系客服
+    callKefu() {
+      wx.makePhoneCall({
+        phoneNumber: "18888888888" // 仅为示例，并非真实的电话号码
+      });
+    },
+    // 选择地址
+    chooseAddress() {
+      // 微信小程序中 也需要上下文绑定
+      // let that = this;
+      wx.chooseAddress({
+        success:(res)=>{
+          // console.log(res.userName);
+          // console.log(res.postalCode);
+          // console.log(res.provinceName);
+          // console.log(res.cityName);
+          // console.log(res.countyName);
+          // console.log(res.detailInfo);
+          // console.log(res.nationalCode);
+          // console.log(res.telNumber);
+          this.address = res.provinceName+res.cityName+res.countyName;
+        }
+      });
+    }
+  },
+  onLoad(options) {
+    //   console.log(options);
+    this.id = options.id;
+    // 获取网络数据
+    hxios.get(`api/public/v1/goods/detail?goods_id=${this.id}`).then(res => {
+      //   console.log(res);
+      this.goodsInfo = res.data.message;
+    });
+  }
+};
+</script>
+<style lang='scss'>
+$uRed: #eb4450;
+
+// 使用padding 让底部的内容 向上
+page {
+  padding-bottom: 90rpx;
+}
+swiper {
+  height: 700rpx;
+}
+swiper-item {
+  image {
+    display: block;
+    width: 100%;
+    height: 700rpx;
+  }
+}
+
+// 商品基本信息
+.goodsinfo {
+  padding: 0 15rpx;
+  box-sizing: border-box;
+  .price {
+    color: $uRed;
+    font-size: 36rpx;
+    padding: 40rpx 0;
+  }
+  .name-box {
+    display: flex;
+    .left {
+      overflow: hidden;
+      text-overflow: ellipsis;
+      display: -webkit-box;
+      -webkit-box-orient: vertical;
+      -webkit-line-clamp: 2;
+      font-size: 32rpx;
+      padding-right: 80rpx;
+    }
+    .right {
+      width: 144rpx;
+      text-align: center;
+      border-left: 1rpx solid gray;
+      font-size: 22rpx;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      .iconfont {
+        display: block;
+        margin: 0 auto;
+      }
+    }
+  }
+  .express {
+    padding: 35rpx 0;
+    color: #ccc;
+    font-size: 25rpx;
+  }
+}
+
+// 底部的控制区域
+.control-box {
+  display: flex;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 90rpx;
+  background: #ccc;
+  .item {
+    flex: 2;
+    text-align: center;
+    font-size: 24rpx;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    .iconfont {
+      display: block;
+      margin: 0 auto;
+    }
+  }
+  button {
+    flex: 3;
+    height: 100%;
+    color: white;
+    border-radius: 0;
+    border: none;
+  }
+  .cart {
+    background-color: #f4b73f;
+  }
+  .buy {
+    background-color: $uRed;
+  }
+}
+
+// 选项区域
+.section {
+  margin-bottom: 10rpx;
+  padding-top: 10rpx;
+  background-color: gray;
+  .item {
+    background-color: white;
+    height: 100rpx;
+    line-height: 100rpx;
+    color: gray;
+    font-size: 30rpx;
+    span {
+      color: black;
+    }
+    .iconfont {
+      display: inline-block;
+    }
+  }
+  .item.hot {
+    color: $uRed;
+  }
+
+  // 微信小程序 并不支持 所有的选择器
+  // &:first-of-type{
+  //   background-color: hotpink;
+  //   .item:first-child{
+  //     color:$uRed;
+  //   }
+  // }
+
+}
+// 自己实现tab栏
+.tab-bar{
+  display: flex;
+  padding-top: 18rpx;
+  background-color: gray;
+  .bar-item{
+    flex:1;
+    background: white;
+    height: 100rpx;
+    line-height: 100rpx;
+    font-size:22rpx;
+    text-align: center;
+    &.active{
+      color:$uRed;
+      border-bottom: 8rpx solid $uRed;
+    }
+  }
+}
+.tab-content{
+  .content-item{}
+}
+
+
+</style>
--- src/pages/search/index.vue
+++ src/pages/search/index.vue
@@ -8,35 +8,179 @@
         @confirm="search"
         v-model.trim="key"
       >
-      <button>取消</button>
+      <button @click="clearKey">取消</button>
     </div>
-    <div class="history">
+    <div
+      class="history"
+      v-show="searchList.length==0"
+    >
       <div class="top">
         <text>历史搜索</text>
-        <i class="iconfont icon-shanchu"> </i>
+        <i
+          @click="clearHistory"
+          class="iconfont icon-shanchu"
+        > </i>
       </div>
       <div class="bottom">
-        <div class="item" v-for="(item, index) in historyList" :key="index">{{item}}</div>
+        <div
+          class="item"
+          v-for="(item, index) in historyList"
+          :key="index"
+          @click="clickSearch(item)"
+        >{{item}}</div>
         <!-- <div class="item">大米</div>
         <div class="item">糯米</div> -->
       </div>
     </div>
+    <div
+      class='search-box'
+      v-show="searchList.length!=0"
+    >
+      <!-- 顶部的排序 -->
+      <div class='order-box'>
+        <div
+          class="option"
+          :class="{active:orderOption==0}"
+          @click="orderOption=0"
+        >
+          综合
+        </div>
+        <div
+          class="option"
+          :class="{active:orderOption==1}"
+          @click="orderOption=1"
+        >
+          销量
+        </div>
+        <div
+          class="option"
+          :class="{active:orderOption==2}"
+          @click="orderOption=2;isUp=!isUp"
+        >
+          价格
+          <div class="arrow-box">
+            <i
+              class='iconfont icon-jiantoushang'
+              :class="{active:isUp}"
+            ></i>
+            <i
+              class='iconfont icon-jiantouxia'
+              :class="{active:!isUp}"
+            ></i>
+          </div>
+        </div>
+      </div>
+      <!-- 底部的结果 需要滚动 -->
+      <scroll-view scroll-y>
+        <!-- 使用计算属性实现 -->
+        <a
+          class="item"
+          v-for="(item, index) in orderList"
+          :key="item.goods_id"
+          :href="'/pages/detail/main?id='+item.goods_id"
+        >
+          <div class="left">
+            <img
+              :src="item.goods_small_logo"
+              alt=""
+            >
+          </div>
+          <div class="right">
+            <div class="title">{{item.goods_name}}</div>
+            <div class="price">{{item.goods_price}}</div>
+          </div>
+        </a>
+      </scroll-view>
+    </div>
   </div>
 </template>
 <script>
+// 导入 hxios
+import hxios from "../../utils/index";
 export default {
   data: function() {
     return {
       // 搜索历史
       historyList: [],
       //   搜索内容
-      key: ""
+      key: "",
+      // 查询的结果
+      searchList: [],
+      // 专门用来排序的数组
+      _searchList:[],
+      // 筛选的条件字段
+      orderOption: 0,
+      // 标记价格排序的字段
+      isUp: false
     };
   },
+  // 计算属性
+  computed: {
+    // 获取排序之后的数组
+    orderList() {
+      // 导入如何排序 取决于
+      if (this.orderOption == 0) {
+        // 综合排序 返回默认数组
+        return this.searchList;
+      } else if (this.orderOption == 1) {
+        // 销量排序 sort是原数组排序
+        // 专门弄一个排序的数组
+        return this._searchList.sort((a, b) => {
+          return a.goods_id - b.goods_id;
+        });
+      }else if(this.orderOption==2){
+        // 升序 和降序
+       return this._searchList.sort((a,b)=>{
+          if(this.isUp==true){
+            return a.goods_price - b.goods_price
+          }else{
+            return b.goods_price - a.goods_price
+          }
+        })
+      }
+    }
+  },
   methods: {
+    // 查询数据
     search() {
-        // console.log(this.key);
-        this.historyList.push(this.key);
+      // 重复内容判断
+      // 判断是否有相同的内容 如果有 删除旧的 再追加新的
+      let index = this.historyList.indexOf(this.key);
+      if (index != -1) {
+        this.historyList.splice(index, 1);
+      }
+      // 长度限制
+      // 长度超过了 5个  每次追加的时候 再删除第一个即可
+      if (this.historyList.length >= 5) {
+        this.historyList.shift();
+      }
+
+      // console.log(this.key);
+      this.historyList.push(this.key);
+      // 网络数据获取 hxios
+      hxios.get(`api/public/v1/goods/search?query=${this.key}`).then(res => {
+        // console.log(res);
+        // 初始数组
+        this.searchList = res.data.message.goods;
+        // 专门弄一个排序的数组
+        this._searchList = JSON.parse(JSON.stringify(this.searchList));
+      });
+    },
+    // 清除历史记录
+    clearHistory() {
+      // 清除历史记录
+      this.historyList = [];
+    },
+    // 点击搜索结果
+    clickSearch(key) {
+      // 填充到搜索框中
+      this.key = key;
+      // 执行搜索
+      this.search();
+    },
+    // 清空搜索
+    clearKey() {
+      this.key = "";
     }
   },
   onShow() {
@@ -67,6 +211,7 @@ export default {
 };
 </script>
 <style lang="scss">
+$uRed: #eb4450;
 .search-container {
   .search {
     height: 120rpx;
@@ -116,6 +261,93 @@ export default {
         margin-right: 30rpx;
         font-size: 26rpx;
         margin-bottom: 20rpx;
+      }
+    }
+  }
+}
+
+// 搜索的排序
+.search-box {
+  .order-box {
+    display: flex;
+    height: 100rpx;
+    border-bottom: 1px solid gray;
+    .option {
+      flex: 1;
+      text-align: center;
+      line-height: 100rpx;
+      font-size: 25rpx;
+      .arrow-box {
+        .iconfont {
+          display: block;
+        }
+      }
+    }
+    .option:last-child {
+      display: flex;
+      justify-content: center;
+      .iconfont {
+        width: 20rpx;
+        height: 15rpx;
+        margin-bottom: 5rpx;
+      }
+    }
+
+    // 高亮的样式
+    .option.active {
+      color: $uRed;
+    }
+    .option.active {
+      .iconfont {
+        color: black;
+      }
+      .iconfont.active {
+        color: $uRed;
+      }
+    }
+  }
+  // 内容区域
+  scroll-view {
+    // 约束死高度
+    height: 1112rpx;
+    padding: 0 20rpx;
+    box-sizing: border-box;
+    .item {
+      height: 260rpx;
+      border-bottom: 1rpx solid gray;
+      display: flex;
+      .left {
+        padding: 30rpx;
+        img {
+          display: block;
+          width: 200rpx;
+          height: 200rpx;
+        }
+      }
+      .right {
+        flex: 1;
+        padding: 30rpx 0;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        .title {
+          text-overflow: -o-ellipsis-lastline;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          display: -webkit-box;
+          -webkit-line-clamp: 2;
+          line-clamp: 2;
+          -webkit-box-orient: vertical;
+          font-size: 28rpx;
+        }
+        .price {
+          font-size: 30rpx;
+          color: $uRed;
+          &::before {
+            content: "¥";
+            font-size: 20rpx;
+          }
+        }
       }
     }
   }
